ea315c5e3463d7b04cff7b3ff17440019cb0593c
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2023-03-24 21:38:05 +1000

    Add HTML templating

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,9 +1,14 @@
-use actix_web::{HttpResponse, Responder};
-
+use actix_web::HttpResponse;
+use askama::Template;
+mod template;
+use template::IndexTemplate;
 pub mod user_handlers;
 
 pub use user_handlers::{get_user, UserId};
 
-pub async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
+pub async fn index() -> actix_web::Result<HttpResponse> {
+    let tmpl = IndexTemplate;
+    tmpl.render()
+        .map(|body| HttpResponse::Ok().content_type("text/html").body(body))
+        .map_err(|_| actix_web::error::ErrorInternalServerError("Template rendering failed"))
 }
